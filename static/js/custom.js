1cb5dcc28c9a4d0c958de2c540d4218628c70d9d
Stanislav Popov <popstas@gmail.com>
2020-06-04 22:25:08 +0500

    feat: кликабельные картинки в списке постов

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.entry-content h2, .entry-content h3, .entry-content h4, .entry-content h5').forEach((h) => {
     h.innerHTML = `${h.innerText} <a class="heading-anchor" href="#${h.id}">🔗</a>`
   });
+
+  document.querySelectorAll('.blog-index article').forEach((art) => {
+    const href = art.querySelector('a').getAttribute('href');
+    const img = art.querySelector('img');
+    if(!img) return;
+
+    const a = document.createElement('a');
+    a.setAttribute('href', href);
+    img.parentNode.insertBefore(a, img);
+    a.appendChild(img);
+  });
 });
