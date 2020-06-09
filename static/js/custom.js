133821298b0e9a315731c66e911217eb32b2827e
Stanislav Popov <popstas@gmail.com>
2020-06-10 00:01:00 +0500

    feat: custom ReadingTime (.Params.time)

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // anchors for headings
   document.querySelectorAll('.entry-content h2, .entry-content h3, .entry-content h4, .entry-content h5').forEach((h) => {
-    h.innerHTML = `${h.innerText} <a class="heading-anchor" href="#${h.id}">🔗</a>`
+    h.innerHTML = `${h.innerText} <a class="heading-anchor" href="#${h.id}">🔗</a>`;
   });
 
   // clickable images in index
